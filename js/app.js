b3965ec8bbd8cbddc5e61f777e2ebcc1995f3db6
Stiven <stiven.cardona.0924@gmail.com>
2022-05-16 14:54:50 -0500

    Calculadora basica (suma)

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log('Aplicación calculadora');
+
+function sumar(){
+
+
+    const forma = document.getElementById('forma');
+
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+
+    if(isNaN(resultado)){
+        resultado = 'La Operación no incluye números';
+    }
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+
+}
